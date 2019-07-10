cd43f84714403f8b2b0f4a92ee65d566d9ecf278
Krishnakumar Arjunan <krishnakumar.arjunan@softvison.com>
2019-07-10 19:56:07 +0530

    node microservice persistance

--- client.js
+++ client.js
@@ -5,32 +5,17 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const fs = require('fs');
 const process = require('process');
-const grpc = require('grpc');
 
+// var protoLoader = require('@grpc/proto-loader');
+const Employee = require('./routes/employeeModel')
 
 //Router configuration
 const employeeRouter = require('./routes/employeeRouter')();
 
 //Application port and ip
 const PORT = 3050;
-// const HOST = '0.0.0.0';
-
-
-// port configuration for GRPC connectivity
-const GRPC_PORT = 53016
-// const GRPC_PORT = 9000;
-
-//proto file path 
-const serviceDef = grpc.load(PROTO_PATH);
 
 const app = express();
-const creds = grpc.credentials.createInsecure()
-// const client = new serviceDef.EmployeeConsumerService(`localhost:${GRPC_PORT}`, creds);
-const client = new serviceDef.EmployeeProducerService(`10.0.102.166:${GRPC_PORT}`, creds);
-// const client = new serviceDef.EmployeeConsumerService(`172.24.235.1:${GRPC_PORT}`, creds);
-const GRPC_HOST = process.env.GRPC_SERVER_IP;
-console.log("GRPC- HOST " + GRPC_HOST);
-// const client = new serviceDef.EmployeeConsumerService(`${GRPC_HOST}`, creds);
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -44,5 +29,4 @@ app.listen(PORT,() => {
     console.log("Running on port " + PORT);
 });
 
-module.exports.client=client;
 
--- routes/employeeRouter.js
+++ routes/employeeRouter.js
@@ -1,26 +1,39 @@
 const express = require('express');
-const client = require('../client'); 
 const Employee = require('./employeeModel')
+// const grpc_client = require('../client');
+const GRPC_PORT = 8000;
+const grpc = require('grpc');
+const PROTO_PATH = 'messages.proto';
+
+//proto file path 
+const serviceDef = grpc.load(PROTO_PATH);
+const creds = grpc.credentials.createInsecure()
+// const client = new serviceDef.EmployeeConsumerService(`localhost:${GRPC_PORT}`, creds);
+const grpc_client = new serviceDef.EmployeeProducerService(`10.0.102.166:${GRPC_PORT}`, creds);
+// const client = new serviceDef.EmployeeConsumerService(`172.24.235.1:${GRPC_PORT}`, creds);
+const GRPC_HOST = process.env.GRPC_SERVER_IP;
+// const client = new serviceDef.EmployeeConsumerService(`${GRPC_HOST}`, creds);
 
 
 function routes() {
     const employeeRouter = express.Router();
     employeeRouter.route('/save')
     .post((req,res) => {
-      console.log("Req =>  " + req);
-      var employee = new Employee(req.body);
-      console.log(employee);
-      delete employee['_id']
-      console.log("After => deleteing id => " + employee);
-      // const call = client.saveAll();
-      // call.on('data', function(emp) {
-      //    console.log(emp.employee);
-      // });
-      // employees.forEach(function (emp) {
-      //    call.write({employee: emp});
-      // });
-      // call.end();
-       return res.json(employee);
+      const employee = new Employee(req.body);
+      var emp = {};
+      emp['id'] = employee.id;
+      emp['badgeNumber'] = employee.badgeNumber;
+      emp['firstName'] = employee.firstName;
+      emp['lastName'] = employee.lastName;
+      console.log(emp);
+        grpc_client.save({employee: emp}, function(err, response) {
+         if (err) {
+             console.log("Logging Error" + err);
+         } else {
+             console.log("Logging Success " + response);
+         }
+     });
+       return res.json("Persisted Successfully");
     });
 
     return employeeRouter;
